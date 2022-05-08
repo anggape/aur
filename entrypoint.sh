de930f5a855802a1c31535ca0182cc3c7a165a91
anggape <anggape.dev@gmail.com>
2022-05-08 20:17:04 +0700

    chore: install aur packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env zsh
 
-yay -V
+if [ -d "yay-cache" ]; then
+    mkdir -p ~${BUILDER_USER}/.cache
+    sudo -u ${BUILDER_USER} mv "yay-cache" ~${BUILDER_USER}/.cache/yay
+fi
+
+sudo -u ${BUILDER_USER} yay -Syu --noconfirm - <aur-packages
+
+cp ~${BUILDER_USER}/.cache/yay ${GITHUB_WORKSPACE}/yay-cache
