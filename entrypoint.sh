511007ab7f0fe70bc364a096ec8122f5d68dd57a
anggape <anggape.dev@gmail.com>
2022-05-09 10:08:57 +0700

    ci: create release

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,25 @@
 
 set -e
 
+function create_tag() {
+    TAG_DATE=$(date '+%Y/%m/%d')
+    if [ -z "$1" ]; then
+        TAG_ID=0
+    else
+        TAG_ID=$((TAG_ID + 1))
+    fi
+
+    RELEASE_TAG="${TAG_DATE}.${TAG_ID}"
+
+    if [ $(git tag -l "${RELEASE_TAG}") ]; then
+        create_tag ${TAG_ID}
+    else
+        echo "::set-output name=RELEASE_TAG::${RELEASE_TAG}"
+        git tag "${RELEASE_TAG}"
+    fi
+}
+create_tag
+
 if [ -d "${YAY_CACHE_DIR}" ]; then
     mkdir -p ~${BUILDER_USER}/.cache
     sudo -u ${BUILDER_USER} cp "${YAY_CACHE_DIR}" ~${BUILDER_USER}/.cache/yay -r
